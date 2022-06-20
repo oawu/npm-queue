4f0ffd900f410db3af8a5ef9c18250721794ec6f
OA Wu <oa.wu@idrip.coffee>
2022-06-20 17:51:23 +0800

    Fix author Fix copyright

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2021, @oawu/queue
+ * @author      OA Wu <oawu.tw@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2022, @oawu/queue
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
